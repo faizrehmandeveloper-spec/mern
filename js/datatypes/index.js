a0d5e428b6abc6dd9e0de4c9ed346820c0386b69
shanakhan474-afk <shanakhan474@gmail.com>
2026-06-30 17:53:10 +0530

    local code push on server

---
+++ js/datatypes/index.js
@@ -0,0 +1,16 @@
+// data types 8
+let number = 42; // number
+let string = "Hello, World!"; // string
+let boolean = true; // boolean
+let nullValue = null; // null
+let undefinedValue; // undefined
+let symbolValue = Symbol("unique"); // symbol
+let bigIntValue = 9007199254740991n; // BigInt
+let objectValue = { name: "Alice", age: 30 }; // object
+
+// string in built functions 
+
+console.log(string.length);
+console.log(string.toUpperCase()); 
+console.log(string.toLowerCase()); 
+console.log(string.trim()); 
---
+++ js/varrible/index.js
@@ -0,0 +1,10 @@
+// 1. redeclare use for var keyword 
+var name = "fazlu rehman";
+var name = "fazlu rehman";
+
+// 2. not redeclare varrible use let keyword but assign  value also 
+let first_var = "fazlu rehman";
+first_var = "testing";
+
+// 3. not redeclare varrible using const keyword and not assign value 
+const first = "fazlu rehman";
